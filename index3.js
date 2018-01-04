62b31883ed5acf0bc0be47bb1d7f5a6889f1f87b
Kyle Kodani <kkodani@users.noreply.github.com>
2018-01-03 16:06:14 -0800

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,5 +1,5 @@
 var requireFromUrl = require('require-from-url/sync');
-var script3 = requireFromUrl('./script3');
+var script3 = requireFromUrl('https://raw.githubusercontent.com/kkodani/hello-world/master/script3.js');
 
 var scripts = {
   script3: script3
